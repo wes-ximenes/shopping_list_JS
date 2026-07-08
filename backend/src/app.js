880a60a297c6f004ca42b3860f346bd6daf0a245
wes-ximenes <wximenes4@gmail.com>
2026-07-08 17:01:42 -0300

    Altera app.js, comments

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,14 @@
-const express = require("express");
+const express = require("express"); // Importa o módulo express, que é um framework para criar nossa API REST.
 
-const app = express();
+const app = express(); //Cria a instância do express, que será usada para definir as rotas e middlewares da nossa API.
 
-// Permite que a API receba dados em JSON
-app.use(express.json());
+app.use(express.json()); // Permite que a API receba dados em JSON
 
 // Rota de teste
-app.get("/", (req, res) => {
+app.get("/", (req, res) => { // Define uma rota GET na raiz da API, que responde com uma mensagem de teste.
     res.json({
         message: "API Lista de Compras funcionando!"
     });
 });
 
-module.exports = app;
+module.exports = app; // Exporta a instância do express para que ela possa ser usada em outros arquivos do projeto.
